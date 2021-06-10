4476648c9858ff7579f705ad5e60be73ff4432ab
Márton Donát Nagy <mdonat.nagy@tum.de>
2021-06-10 12:33:57 +0200

    implement metric DistortHugeUpRandom

--- code/behavior.py
+++ code/behavior.py
@@ -94,3 +94,12 @@ class DistortUpByOneAlways(DistortStrategy):
 class DistortUpByOneRandom(DistortStrategy):
     def distort(self, distorter, measured_truth, random_seed=None):
         return force_agent_exposed_bounds( measured_truth + self.rng(random_seed).randint(0,1) )
+
+class DistortHugeUpRandom(DistortStrategy):
+    def distort(self, distorter, measured_truth, random_seed=None):
+        leeway = config.get("MAX_RATING") - measured_truth
+        distorted_truth = measured_truth + self.rng(random_seed).uniform(
+            leeway/2,
+            leeway)
+        distorted_truth = round(distorted_truth, config.get("DECIMAL_PRECISION"))
+        return force_agent_exposed_bounds( distorted_truth )
