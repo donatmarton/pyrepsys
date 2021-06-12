ea2e303f9cf68d87bedc2859211b727a3925b5a3
Márton Donát Nagy <mdonat.nagy@tum.de>
2021-06-12 17:45:41 +0200

    cli entry can call simulation or test suite

--- pyrepsys/pyrepsys-cli.py
+++ pyrepsys/pyrepsys-cli.py
@@ -1,5 +1,43 @@
+import argparse
 
 import pyrepsys
 
 
-pyrepsys.run()
+def run_test(args):
+    import pytest
+    pytest.main(["tests/"])
+
+def run_sim(args):
+    pyrepsys.run()
+
+
+parser = argparse.ArgumentParser(
+    epilog="See 'pyrepsys-cli <command> -h'"
+)
+
+subparsers = parser.add_subparsers(
+    title="commands",
+    description="pyrepsys can be launched in these modes:",
+    required=True,
+    metavar="{simulate|test}"
+    )
+
+# create the parser for the "simulate" command
+parser_sim = subparsers.add_parser(
+    "simulate",
+    aliases=["s", "sim"],
+    help="Simulate selected scenarios",
+    description="Simulate selected scenarios")
+parser_sim.set_defaults(selected_mode=run_sim)
+
+# create the parser for the "test" command
+parser_test = subparsers.add_parser(
+    "test",
+    aliases=["t"],
+    help="Run all self-tests",
+    description="Run all self-tests")
+parser_test.set_defaults(selected_mode=run_test)
+
+# parse args and call given command
+args = parser.parse_args()
+args.selected_mode(args)
