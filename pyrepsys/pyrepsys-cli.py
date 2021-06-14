74a077eefdb0290490132c3c9363c1068106ed26
Márton Donát Nagy <mdonat.nagy@tum.de>
2021-06-14 17:07:49 +0200

    cli simulation supports runparams or scenario list

--- pyrepsys/pyrepsys-cli.py
+++ pyrepsys/pyrepsys-cli.py
@@ -8,7 +8,10 @@ def run_test(args):
     pytest.main(["tests/"])
     
 def run_sim(args):
-    pyrepsys.run(args.runparams)
+    if args.runparams:
+        pyrepsys.run(args.runparams)
+    else:
+        pyrepsys.run(scenario_list=args.scenario, scenario_defaults=args.default_scenario)
 
 def yaml_file(string: str) -> str:
     parts = string.split(".")
@@ -19,30 +22,41 @@ def yaml_file(string: str) -> str:
 
 parser = argparse.ArgumentParser(
     epilog="See 'pyrepsys-cli <command> -h'"
-    )
+)
 
 subparsers = parser.add_subparsers(
     title="commands",
     description="pyrepsys can be launched in these modes:",
     required=True,
     metavar="{simulate|test}"
-    )
+)
 
 # create the parser for the "simulate" command
 parser_sim = subparsers.add_parser(
     "simulate",
-    aliases=["s", "sim"],
+    aliases=["sim", "s"],
     help="Simulate selected scenarios",
     description="Simulate selected scenarios"
-    )
+)
 parser_sim.set_defaults(selected_mode=run_sim)
-parser_sim.add_argument(
-    "runparams",
+scenario_arg_group = parser_sim.add_mutually_exclusive_group(required=True)
+scenario_arg_group.add_argument(
+    "-rp", "--runparams",
     nargs="?",
     type=yaml_file,
-    default="run_params.yaml",
     help="Name of the YAML file with the simulation run parameters"
-    )
+)
+scenario_arg_group.add_argument(
+    "-s", "--scenario",
+    nargs="*",
+    type=yaml_file,
+    help="Name of the YAML file(s) with scenarios to simulate"
+)
+parser_sim.add_argument(
+    "-d", "--default-scenario",
+    type=yaml_file,
+    help="Name of the YAML file with the default scenario settings. Required if scenarios are given as arguments"
+)
 
 # create the parser for the "test" command
 parser_test = subparsers.add_parser(
@@ -53,6 +67,14 @@ parser_test = subparsers.add_parser(
     )
 parser_test.set_defaults(selected_mode=run_test)
 
-# parse args and call given command
+# parse args
 args = parser.parse_args()
+
+# post-parsing argument processing
+if args.scenario is not None and args.default_scenario is None:
+    parser_sim.error("the following arguments are required with -s/--scenario: -d/--default-scenario")
+if args.runparams and args.default_scenario:
+    parser_sim.error("the following arguments are not allowed with -rp/--runparams: -d/--default-scenario")
+
+# call given command
 args.selected_mode(args)
